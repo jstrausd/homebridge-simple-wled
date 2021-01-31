a6795cfe09944688b00c87fcbfbff540bbb6952f
Jonathan Strauss <office@jstrauss.at>
2021-01-31 22:29:18 +0100

    Fixed caching issue

--- src/wled-accessory.ts
+++ src/wled-accessory.ts
@@ -72,7 +72,7 @@ export class WLED {
     this.api = platform.api;
     this.hap = this.api.hap;
     this.Characteristic = this.api.hap.Characteristic;
-    const uuid = this.api.hap.uuid.generate('homebridge:wled' + this.name + Math.random());
+    const uuid = this.api.hap.uuid.generate('homebridge:wled' + this.name);
 
     if ((this.wledAccessory = this.platform.accessories.find((x: PlatformAccessory) => x.UUID === uuid)!) === undefined) {
 
